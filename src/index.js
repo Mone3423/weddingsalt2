f1d2b12dd3885a1cc66b0ca6c788fad574f9e741
Monserrat Hidalgo <153001846+Mone3423@users.noreply.github.com>
2025-05-21 16:55:22 -0400

    .

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './App'; // Importa el router que definiste en App.js
-import './index.css'; // Si tienes estilos globales
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
